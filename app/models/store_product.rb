203f23896c88a16717f757a3b1406643b7e9d20a
Arif <arif@pragmakw.com>
2024-10-20 13:42:28 +0530

    fixed products

--- app/models/store_product.rb
+++ app/models/store_product.rb
@@ -4,6 +4,7 @@ class StoreProduct < ApplicationRecord
   enum state: { available: 0, in_use: 1 }
   belongs_to :product
   belongs_to :store
+  belongs_to :brand
   after_save :update_product_count
   after_destroy :update_product_count
 
