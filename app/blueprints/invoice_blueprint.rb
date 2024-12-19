282ab7d45cae6bd9fc04caf1427f5d3e6b8d28ed
snlkumar <sunil@ezzie.in>
2024-12-19 11:43:56 +0530

    updated limit (#31)

--- app/blueprints/invoice_blueprint.rb
+++ app/blueprints/invoice_blueprint.rb
@@ -3,7 +3,7 @@
 class InvoiceBlueprint < Blueprinter::Base
   identifier :id
   field :attachment_name do |invoice|
-    invoice.attachment.filename
+    invoice.attachment.name
   end
 
   fields :invoice_no, :products_count, :imported_count, :failed_count, :error_message, :state
