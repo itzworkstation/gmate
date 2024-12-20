a0209e591b43749b128d3ff931a0713e93a3f73e
snlkumar <sunil@ezzie.in>
2024-12-20 14:10:18 +0530

    added delete store and started date in invoice api (#32)

--- app/blueprints/invoice_blueprint.rb
+++ app/blueprints/invoice_blueprint.rb
@@ -6,5 +6,9 @@ class InvoiceBlueprint < Blueprinter::Base
     invoice.attachment.name
   end
 
+  field :created_at do |invoice|
+    (DateTime.now - rand(10).days).strftime("%d-%b-%Y %I:%M %p")
+  end
+
   fields :invoice_no, :products_count, :imported_count, :failed_count, :error_message, :state
 end
