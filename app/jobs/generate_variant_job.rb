427cbe82ba2e88a70ec89d62ffa96e8c0428cb05
sunil kumar <snlkumar447@gmail.com>
2024-12-27 15:40:50 +0530

    enabled for account and category

--- app/jobs/generate_variant_job.rb
+++ app/jobs/generate_variant_job.rb
@@ -2,13 +2,13 @@
 class GenerateVariantJob < BaseJob
   sidekiq_options unique: :until_executed, lock: :until_executed
   # call it UploadInvoiceJob.perform_later(2)
-  def perform(cat_id)
-    cat =Category.find cat_id
-    cat.image.variant(:thumbnail).processed
+  def perform(obj_id, klass)
+    obj =klass.classify.constantize.find obj_id
+    obj.image.variant(:thumbnail).processed
   end
   private
 
   def find_job_statusable(job)
-    Category.find(job.arguments.first)  # You can adjust this logic based on your actual job arguments
+    job.arguments.last.classify.constantize.find(job.arguments.first)  # You can adjust this logic based on your actual job arguments
   end
 end
